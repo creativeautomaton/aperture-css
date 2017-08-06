3a21d55317e85edaeb577a7bfdbae7b763c98fac
deepmind <creativeautomaton@gmail.com>
2017-08-05 22:14:21 -0500

    added minify and npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass'); 
-var uglify = require('gulp-uglify');
-var pump = require('pump');
-
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 var paths = {
   sass: ['./**/*.scss']
 };
 
+gulp.task('minify-css',() => {
+  return gulp.src('aperture.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('aperture.min.css'));
+});
+
+// gulp.task('minify-css', () => {
+//   return gulp.src('aperture.css')
+//     .pipe(cleanCSS({compatibility: 'ie8'}))
+//     .pipe(gulp.dest('aperture.min.css'));
+// });
+
 // Runs on 'gulp'
-gulp.task('default', ['minify'], function() { });
+gulp.task('default', ['minify-css'], function() { });
