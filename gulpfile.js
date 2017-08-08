0faaa8cdb6c7d2f24722ca479855fda783d1d564
deepmind <creativeautomaton@gmail.com>
2017-08-08 06:38:38 -0500

    latest

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
+
 var paths = {
   sass: ['./**/*.scss']
 };
@@ -10,14 +11,8 @@ gulp.task('minify-css',() => {
     .pipe(sourcemaps.init())
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('aperture.min.css'));
+    .pipe(gulp.dest('dist'));
 });
-
-// gulp.task('minify-css', () => {
-//   return gulp.src('aperture.css')
-//     .pipe(cleanCSS({compatibility: 'ie8'}))
-//     .pipe(gulp.dest('aperture.min.css'));
-// });
 
 // Runs on 'gulp'
 gulp.task('default', ['minify-css'], function() { });
